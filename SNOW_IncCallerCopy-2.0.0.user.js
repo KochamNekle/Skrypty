b8e215e4847cea8d4afd68b411be102c903b3d58
KochamNekle <krzysztof.zielonka@jeronimo-martins.com>
2025-05-14 15:38:42 +0200

    Add files via upload

---
+++ SNOW_IncCallerCopy-2.0.0.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         SNOW_IncCallerCopy
+// @namespace    https://github.com/KochamNekle/Skrypty
+// @version      2.0.0
+// @description  Kopiowanie numeru incydentu oraz numeru sklepu po kliknięciu
+// @author       Krzysztof Zielonka
+// @match        *://www.jm-hello.com/*
+// @updateURL    https://raw.githubusercontent.com/KochamNekle/Skrypty/main/SNOW_INC_Caller_Copy.user.js
+// @downloadURL  https://raw.githubusercontent.com/KochamNekle/Skrypty/main/SNOW_INC_Caller_Copy.user.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var INCNumber = document.getElementById("sys_readonly.incident.number");
+    var INCOpenedBy = document.getElementById("incident.opened_by_label");
+    var CallerNumber = document.getElementById("sys_display.incident.caller_id");
+
+    function copyWithoutSpaces(element) {
+        var copyText = element.value.replace(/\s+/g, '');
+        navigator.clipboard.writeText(copyText);
+    }
+
+    INCNumber.onclick = function() {
+        copyWithoutSpaces(INCNumber);
+    }
+    INCOpenedBy.onclick = function() {
+        if (INCOpenedBy.hasAttribute('readonly')) {
+        copyWithoutSpaces(INCOpenedBy);
+        }
+    }
+    CallerNumber.onclick = function() {
+        if (CallerNumber.hasAttribute('readonly')) {
+        copyWithoutSpaces(CallerNumber);
+        }
+    }
+})();
